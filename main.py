87bb48056fd2ca735a7910a4690c9ed33e623b85
Aaron Sahar <aaronsahar@gmail.com>
2022-10-09 23:55:14 -0700

    are fnare

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-
-words = input('Enter the list: ').split()
-
+words = input("Enter the list: ").split()
 are = ['a', 'r', 'e']
-idxlst = []
-
-# ******************************
-# Make your Code
-# ******************************
-
-# print the words that has 'a', 'r', 'e' in sequence
-
+are_input_list = []
+for i in range(len(words)):
+    idxlst = [words[i].find(are[j]) for j in range (len(are))]
+    if idxlst[0] >= 0 and idxlst[1] >= 0 and idxlst[2] >= 0:
+        are_input_list.append(words[i])
+for i in range(len(are_input_list)):
+    print(are_input_list[i], end=' ')
